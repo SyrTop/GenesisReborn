429b5ddf5bffddda5184afcd633a13c916daaa08
SyrTop <147376827+SyrTop@users.noreply.github.com>
2024-03-18 09:42:40 +0200

    Update 77_25FKQBAWPV9R3.lua

--- Alpha/77_25FKQBAWPV9R3.lua
+++ Alpha/77_25FKQBAWPV9R3.lua
@@ -1,1 +1,1 @@
-game.Players:FindFirstChild:Kick('Lapha Test ended. Join discord to be notified when the beta starts. https://dsc.gg/genesis-z')
+game.Players:LocalPlayers:Kick('Alpha Test ended. Join discord to be notified when the beta starts. https://dsc.gg/genesis-z')
